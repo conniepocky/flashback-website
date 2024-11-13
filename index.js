cb3143acf829864651223a3c58a81548b9a004e1
Connie <c.amer@icloud.com>
2024-11-13 18:56:12 +0000

    fix images + add final section

--- index.js
+++ index.js
@@ -37,3 +37,37 @@ $(document).ready(function() {
     $temp.remove();
   });
 });
+
+$(".back").css({
+  rotateX: '-180deg'
+});
+$(".prompts").css({
+  transformStyle: "preserve-3d",
+  perspective: '150px',
+  rotateX: 0,
+  width: $(".front").width(),
+  height: $(".front").height()
+});
+$("h3").css({
+  backfaceVisibility: "hidden",
+  perspective: '150px'
+});
+
+var words = [
+  "A winters day", "A friend who always makes me laugh", "A song that brings back memories", "Kaleidoscope",
+  "A favorite New Year’s Eve moment", "A moment I felt proud of someone close to me", "What era do you wish you had lived in?", "A time I felt incredibly lucky"],
+  angle = 0;
+
+setInterval(function (){
+  angle -= 180;
+  $('.prompts').transition({
+      rotateX: '-=180deg'
+  }, 600);
+  
+  var currentB = "." + ((angle / 180) % 2 ? "front" : "back"),
+      current = "." + ((angle / 180) % 2 ? "back" : "front");
+  $(current).html(words[(Math.random() * 8) | 0]);
+  $(".prompts").transition({
+      width: $(current).width()
+  }, 200);
+}, 1300);
